547d01199fbd0fcef824a98d06c69f98f831db5b
Norbert Parti <norbertparti07@gmail.com>
2024-04-29 16:43:05 +0200

    Update NewPost component to use prop types and pass onChange handlers

--- src/component/NewPost.jsx
+++ src/component/NewPost.jsx
@@ -1,27 +1,26 @@
-import { useState } from "react";
-
 import classes from "./NewPost.module.css";
 
-const NewPost = () => {
-  const [body, setBody] = useState('');
+import PropTypes from "prop-types";
 
-  function bodyChangeHandler(event) {
-    setBody(event.target.value);
-  }
-
+const NewPost = (props) => {
   return (
     <form className={classes.form}>
       <p>
         <label htmlFor="body">Text</label>
-        <textarea id="body" required rows={3} onChange={bodyChangeHandler}/>
-        <p>{body}</p>
+        <textarea id="body" required rows={3} onChange={props.onBodyChange}/>
       </p>
       <p>
         <label htmlFor="name">Your name</label>
-        <input type="text" id="name" required />
+        <input type="text" id="name" required onChange={props.onAuthorChange} />
       </p>
     </form>
   );
 };
+
+NewPost.propTypes = {
+  onBodyChange: PropTypes.func.isRequired,
+  onAuthorChange: PropTypes.func.isRequired
+};
+
 
 export default NewPost;
--- src/component/PostList.jsx
+++ src/component/PostList.jsx
@@ -1,15 +1,30 @@
 import classes from "./PostList.module.css";
 
+import { useState } from "react";
+
 import Post from "./Post";
 import NewPost from "./NewPost";
 
 const PostList = () => {
+
+
+    const [body, setBody] = useState("");
+    const [author, setAuthor] = useState("");
+
+    function onBodyChange(event) {
+        setBody(event.target.value);
+    }
+
+    function authorChangeHandler(event) {
+        setAuthor(event.target.value);
+    }
+
     return (
         <div>
-            <NewPost />
+            <NewPost onBodyChange={onBodyChange} onAuthorChange={authorChangeHandler}/>
             <ul className={classes.posts}>
-            <Post author='Norbi' body='Lorem Ipsum' />
-            <Post author='Betti' body='Lorem Ipsum' />
+            <Post author={author} body={body} />
+            <Post author="Betti" body="Lorem ipsuem" />
             </ul>
         </div>
     );
